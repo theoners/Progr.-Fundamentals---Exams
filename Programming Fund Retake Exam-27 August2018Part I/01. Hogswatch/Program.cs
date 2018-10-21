6da183e65ccc2810c0e6bf875f7ce7a7b8811eee
theoners <theonebgrs@gmail.com>
2018-10-21 21:36:01 +0200

    add Programming Fund Retake Exam-27 August 2018 Part I

---
+++ Programming Fund Retake Exam-27 August2018Part I/01. Hogswatch/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _01._Hogswatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int homesToVisit = int.Parse(Console.ReadLine());
+            int numberOfPresents = int.Parse(Console.ReadLine());
+            int totalPresents = numberOfPresents;
+            int visitedHome = 0;
+            int timesBack = 0;
+            int retakeNumberOfPresents = 0;
+
+            for (int i = 0; i < homesToVisit; i++)
+            {
+                int numberOfChildren = int.Parse(Console.ReadLine());
+                if (numberOfChildren<=numberOfPresents)
+                {
+                    visitedHome++;
+                    numberOfPresents -= numberOfChildren;
+                }
+                else
+                {
+                    visitedHome++;
+                    int presentsNeed=((totalPresents / visitedHome) * (homesToVisit-visitedHome) 
+                                                + (numberOfChildren-numberOfPresents));
+                    retakeNumberOfPresents += presentsNeed;
+                    numberOfPresents += presentsNeed;
+                    numberOfPresents -= numberOfChildren;
+                    timesBack++;
+                }
+                
+            }
+
+            if (timesBack==0)
+            {
+                Console.WriteLine(numberOfPresents);
+            }
+            else
+            {
+                Console.WriteLine(timesBack);
+                Console.WriteLine(retakeNumberOfPresents);
+            }
+        }
+    }
+}
---
+++ Programming Fund Retake Exam-27 August2018Part I/02. Grains of Sands/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Grains_of_Sands
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> grainOfSands = Console.ReadLine().Split().Select(int.Parse).ToList();
+            string[] command = Console.ReadLine().Split();
+
+            while (command[0]!="Mort")
+            {
+                if (command[0]=="Add")
+                {
+                    grainOfSands.Add(int.Parse(command[1]));
+                }
+                else if (command[0] == "Remove")
+                {
+                   bool isRomeve= grainOfSands.Remove(int.Parse(command[1]));
+
+                    if (!isRomeve)
+                    {
+                        if (int.Parse(command[1])<grainOfSands.Count)
+                        {
+                            grainOfSands.RemoveAt(int.Parse(command[1]));
+                        }
+                    }
+
+                }
+                else if (command[0] == "Replace")
+                {
+                    if (grainOfSands.Contains(int.Parse(command[1])))
+                    {
+                        int index =grainOfSands.IndexOf(int.Parse(command[1]));
+                        grainOfSands[index] = int.Parse(command[2]);
+                    }
+                }
+                else if (command[0] == "Increase")
+                {
+                    int greaterValue=int.MinValue;
+
+                    foreach (var sands in grainOfSands)
+                    {
+                        if (sands >= int.Parse(command[1]))
+                        {
+                            greaterValue = sands;
+                            break;
+                        }
+                    }
+
+                    if (greaterValue!=int.MinValue)
+                    {
+                        for (int i = 0; i < grainOfSands.Count; i++)
+                        {
+                            grainOfSands[i] = grainOfSands[i] + greaterValue;
+                        }
+                    }
+                    else
+                    {
+                        for (int i = 0; i < grainOfSands.Count; i++)
+                        {
+                            grainOfSands[i] = grainOfSands[i] + grainOfSands[grainOfSands.Count-1];
+                        }
+                    }
+                }
+                else if (command[0] == "Collapse")
+                {
+                    for (int i = 0; i < grainOfSands.Count; i++)
+                    {
+                        if (grainOfSands[i]<int.Parse(command[1]))
+                        {
+                            grainOfSands.RemoveAt(i);
+                            i--;
+                        }
+                    }
+                }
+                command = Console.ReadLine().Split();
+            }
+
+            Console.WriteLine(string.Join(" ",grainOfSands));
+        }
+    }
+}
