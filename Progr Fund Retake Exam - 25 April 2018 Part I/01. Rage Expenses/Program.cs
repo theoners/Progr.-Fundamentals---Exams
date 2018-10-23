e4c3eba9c556e054b7f95bc558c31a92cf52aa42
theoners <theonebgrs@gmail.com>
2018-10-23 17:03:38 +0200

    added Progr Fund Retake Exam - 25 April 2018 Part I

---
+++ Progr Fund Retake Exam - 25 April 2018 Part I/01. Rage Expenses/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _01._Rage_Expenses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lostGameCount = int.Parse(Console.ReadLine());
+            double headsetPrice = double.Parse(Console.ReadLine());
+            double mousePrice = double.Parse(Console.ReadLine());
+            double keyboardPrice = double.Parse(Console.ReadLine());
+            double displayPrice = double.Parse(Console.ReadLine());
+            int headset = 0;
+            int mouse = 0;
+            int keyboard = 0;
+            int display = 0;
+
+            for (int i = 1; i <= lostGameCount; i++)
+            {
+                if (i%2==0)
+                {
+                    headset++;
+                }
+                if (i%3==0)
+                {
+                    mouse++;
+                }
+                if (i%2==0&& i%3==0)
+                {
+                    keyboard++;
+                    if (keyboard % 2 == 0 && keyboard != 0)
+                    {
+                        display++;
+                    }
+                }
+
+                
+            }
+
+            double totalExpenses = headsetPrice * headset + mousePrice * mouse + keyboardPrice * keyboard + displayPrice * display;
+            Console.WriteLine($"Rage expenses: {totalExpenses:F2} lv.");
+        }
+    }
+}
---
+++ Progr Fund Retake Exam - 25 April 2018 Part I/02. Memory View/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Memory_View
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string codeAsText= "";
+            while (input != "Visual Studio crash")
+            {
+                 codeAsText+= input;
+                codeAsText += " ";
+                input = Console.ReadLine();
+            }
+            if (codeAsText == "")
+            {
+                return;
+            }
+            codeAsText = codeAsText.Remove(codeAsText.Length - 1);
+            List<char> name = new List<char>();
+            List<int> code = codeAsText.Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+            while (code.Contains(32656))
+            {
+                int startIndex = code.IndexOf(32656);
+                int letterCount = 0;
+                if (code[startIndex+1]== 19759 && code[startIndex + 2]== 32763)
+                {
+                    letterCount = code[startIndex + 4];
+                    for (int i = startIndex + 6; i < startIndex + 6 +letterCount; i++)
+                    {
+                        name.Add((char)code[i]);
+                    }
+                }
+                for (int i = 0; i < name.Count; i++)
+                {
+                    Console.Write(name[i]);
+                }
+                Console.WriteLine();
+                code.RemoveRange(startIndex, 6 + letterCount);
+                name = new List<char>();
+            }
+
+            
+        }
+    }
+}
