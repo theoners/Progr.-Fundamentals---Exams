821f9b32b8fe5a21c6ca842c71fc5a1031834c4b
theoners <theonebgrs@gmail.com>
2018-12-08 20:45:49 +0100

    added Programming Fund - 28 October 2018 Part II

---
+++ Programming Fund - 28 October 2018 Part II/03. Chore Wars/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _03._Chore_Wars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dishesRegex = new Regex(@"<[a-z0-9]+>");
+            var houseRegex = new Regex(@"\[[A-Z0-9]+\]");
+            var laundryRegex = new Regex(@"\{.+\}");
+            var doingTheDishesTime = 0;
+            var cleaningTheHouseTime = 0;
+            var doingTheLaundryTime = 0;
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+                if (input == "wife is happy")
+                {
+                    break;
+                }
+
+                if (dishesRegex.IsMatch(input))
+                {
+                    var firstMatches = dishesRegex.Matches(input);
+                    var currentTime = CountTime(firstMatches);
+                    doingTheDishesTime += currentTime;
+                }
+
+                if (houseRegex.IsMatch(input))
+                {
+                    var secondMatches = houseRegex.Matches(input);
+                    var currentTime = CountTime(secondMatches);
+                    cleaningTheHouseTime += currentTime;
+                }
+
+                if (laundryRegex.IsMatch(input))
+                {
+                    var thirdMatches = laundryRegex.Matches(input);
+                    var currentTime = CountTime(thirdMatches);
+                    doingTheLaundryTime += currentTime;
+                }
+            }
+
+            Console.WriteLine($"Doing the dishes - {doingTheDishesTime} min.");
+            Console.WriteLine($"Cleaning the house - {cleaningTheHouseTime} min.");
+            Console.WriteLine($"Doing the laundry - {doingTheLaundryTime} min.");
+            Console.WriteLine($"Total - {doingTheLaundryTime + doingTheDishesTime + cleaningTheHouseTime} min.");
+        }
+
+        private static int CountTime(MatchCollection firstMatches)
+        {
+            var time = 0;
+
+            foreach (Match match in firstMatches)
+            {
+                var currentMatch = match.ToString();
+                for (int i = 0; i < currentMatch.Length; i++)
+                {
+                    if (char.IsDigit(currentMatch[i]))
+                    {
+                        time += currentMatch[i]-48;
+                    }
+                }
+            }
+            return time;
+        }
+    }
+}
---
+++ Programming Fund - 28 October 2018 Part II/04. MeTube Statistics/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._MeTube_Statistics
+{
+    public class Video
+    {
+        public string Name { get; set; }
+        public int View { get; set; }
+        public int Like { get; set; } 
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string,Video> videoList = new Dictionary<string, Video>();
+
+            while (true)
+            {
+                var command = Console.ReadLine().Trim();
+                if (command=="stats time")
+                {
+                    break;
+                }
+
+                if (command.Contains('-'))
+                {
+                    var input = command.Split('-', StringSplitOptions.RemoveEmptyEntries);
+                    var name = input[0].Trim();
+                    var views = int.Parse(input[1].Trim());
+                    if (!videoList.ContainsKey(name))
+                    {
+                        
+                        var video = new Video()
+                        {
+                            Name = name,
+                            View = views,
+                            Like = 0
+                        };
+
+                      videoList.Add(name,video);
+                    }
+                    else
+                    {
+                        videoList[name].View += views;
+                    }
+                }
+                else
+                {
+                    var input = command.Split(':',StringSplitOptions.RemoveEmptyEntries);
+                    var likeOrDislike = input[0].Trim();
+                    var videoName = input[1].Trim();
+                    if (videoList.ContainsKey(videoName))
+                    {
+                        if (likeOrDislike == "like")
+                        {
+                            videoList[videoName].Like += 1;
+                        }
+                        else
+                        {
+                            videoList[videoName].Like -= 1;
+                        }
+                    }
+                    
+                }
+            }
+
+            var sortBy = Console.ReadLine().Trim();
+
+            if (sortBy== "by likes")
+            {
+                foreach (var video in videoList.OrderByDescending(x=>x.Value.Like))
+                {
+                    Console.WriteLine($"{video.Value.Name} - { video.Value.View} views - { video.Value.Like} likes");
+                }
+            }
+            else
+            {
+                foreach (var video in videoList.OrderByDescending(x => x.Value.View))
+                {
+                    Console.WriteLine($"{video.Value.Name} - { video.Value.View} views - { video.Value.Like} likes");
+                }
+            }
+
+        }
+    }
+}
