bd8eba3d2e4fed6290124fe7f08427ff2cc21595
theoners <theonebgrs@gmail.com>
2018-12-08 19:08:23 +0100

    added Programming Fund Exam - 28 October 2018 Part I

---
+++ Programming Fund Exam - 28 October 2018 Part I/01. Google Searches/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace _01._Google_Searches
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var totalDays = int.Parse(Console.ReadLine());
+            var numberOfUsers = int.Parse(Console.ReadLine());
+            var moneyPerSearch = double.Parse(Console.ReadLine());
+            var totalMoneyEarned = 0d;
+
+            for (int i = 1; i <= numberOfUsers; i++)
+            {
+                var currentWordLength = double.Parse(Console.ReadLine());
+
+                if (currentWordLength <= 5 && currentWordLength != 1 && i % 3 != 0)
+                {
+                    totalMoneyEarned += moneyPerSearch;
+                }
+                else if (currentWordLength == 1 && i % 3 != 0)
+                {
+                    totalMoneyEarned += moneyPerSearch * 2;
+                }
+
+                else if (i % 3 == 0 && currentWordLength <= 5&& currentWordLength!=1)
+                {
+                    totalMoneyEarned += moneyPerSearch * 3;
+                }
+                else if (currentWordLength == 1 && i % 3 == 0)
+                {
+                    totalMoneyEarned += moneyPerSearch * 6;
+                }
+
+            }
+
+            totalMoneyEarned *= totalDays;
+
+            Console.WriteLine($"Total money earned for {totalDays} days: {totalMoneyEarned:F2}");
+        }
+    }
+}
---
+++ Programming Fund Exam - 28 October 2018 Part I/02. Chat Logger/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02._Chat_Logger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var message = new List<string>();
+
+            while (true)
+            {
+                var line = Console.ReadLine().Split();
+                var command = line[0];
+                if (command=="end")
+                {
+                    break;
+                }
+
+                switch (command)
+                {
+                    case "Chat":
+                        message.Add(line[1]);
+                        break;
+                    case "Delete":
+                    {
+                        while(message.Contains(line[1]))
+                        {
+                            message.Remove(line[1]);
+                        }
+
+                        break;
+                    }
+                    case "Edit":
+                    {
+                        while (message.Contains(line[1]))
+                        {
+                            var index= message.IndexOf(line[1]);
+                            message[index] = line[2];
+                        }
+
+                        break;
+                    }
+                    case "Pin":
+                    {
+                        if (message.Contains(line[1]))
+                        {
+                            message.Remove(line[1]);
+                            message.Add(line[1]);
+                        }
+
+                        break;
+                    }
+                    case "Spam":
+                    {
+                        for (int i = 1; i < line.Length; i++)
+                        {
+                            message.Add(line[i]);
+                        }
+
+                        break;
+                    }
+                }
+            }
+
+            foreach (var word in message)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
