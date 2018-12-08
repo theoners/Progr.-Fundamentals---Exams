377e865d26865c6c3194ff4f48b0bbf5345ef0c0
theoners <theonebgrs@gmail.com>
2018-12-08 22:42:42 +0100

    added Programming Fund- 27 August 2018 Part II

---
+++ Programming Fund- 27 August 2018 Part II/03. Post Office/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _03._Post_Office
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split('|');
+            var firstPart = input[0];
+            var secondPart = input[1];
+            var wordList = input[2].Split();
+            var  result = new List<string>();
+
+            Regex firstPartRegex = new Regex(@"([#$%*&])([A-Z]+)\1");
+            Regex secondPartRegex = new Regex(@"([6-9][0-9]):([0-9]{2,})");
+            var letters = firstPartRegex.Match(firstPart).ToString();
+            var wordLeght = secondPartRegex.Matches(secondPart);
+            foreach (Match count in wordLeght)
+            {
+                var letter = (char)(int.Parse(count.Groups[1].ToString()));
+                if (letters.Contains(letter))
+                {
+                    for (int i = 0; i < wordList.Length; i++)
+                    {
+                        if (wordList[i][0]==letter)
+                        {
+                            if (wordList[i].Length ==1+int.Parse(count.Groups[2].ToString()))
+                            {
+                                if (!result.Contains(wordList[i]))
+                                {
+                                    result.Add(wordList[i]);
+                                }
+                            }
+                            
+                        }
+                    }
+                }
+            }
+
+            for (int i = 1; i < letters.Length-1; i++)
+            {
+                for (int j = 0; j < result.Count; j++)
+                {
+                    if (result[j][0]==letters[i])
+                    {
+                        Console.WriteLine(result[j]);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Programming Fund- 27 August 2018 Part II/04. Iron Girder/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Iron_Girder
+{
+    public class Track
+    {
+       public string TownName { get; set; }
+        public int Time { get; set; }
+        public int Passengers { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new Dictionary<string, Track>();
+
+            while (true)
+            {
+                string line = Console.ReadLine();
+                if (line== "Slide rule")
+                {
+                    break;
+                }
+                var input = line.Split(new string[] {"->",":"}, StringSplitOptions.RemoveEmptyEntries);
+                var town = input[0];
+                var passengers = int.Parse(input[2]);
+                if (input[1]=="ambush")
+                {
+                    if (list.ContainsKey(town))
+                    {
+                        list[town].Time = 0;
+                        list[town].Passengers -= passengers;
+                        if (list[town].Passengers<0)
+                        {
+                            list[town].Passengers = 0;
+                        }
+                    }
+                }
+                else
+                {
+                    var time = int.Parse(input[1]);
+
+
+                    if (!list.ContainsKey(town))
+                    {
+                        var track = new Track()
+                        {
+                            TownName = town,
+                            Time = time,
+                            Passengers = passengers
+                        };
+
+                        list.Add(town, track);
+                    }
+                    else
+                    {
+                        if (list[town].Time > time)
+                        {
+                            list[town].Time = time;
+                        }
+                        else if(list[town].Time==0)
+                        {
+                            list[town].Time = time;
+                        }
+
+                        list[town].Passengers += passengers;
+                    }
+                }
+            }
+
+            foreach (var kvp in list.OrderBy(t=>t.Value.Time).ThenBy(x=>x.Value.TownName))
+            {
+                if (kvp.Value.Time!=0 && kvp.Value.Passengers!=0)
+                {
+                    Console.WriteLine($"{kvp.Value.TownName} -> Time: {kvp.Value.Time} -> Passengers: {kvp.Value.Passengers}");
+                }
+            }
+        }
+
+    }
+}
