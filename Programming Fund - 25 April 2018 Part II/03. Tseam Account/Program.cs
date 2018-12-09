bc8f76a1d6f1f71c00fdd9e678408350ea6181ca
theoners <theonebgrs@gmail.com>
2018-12-09 15:17:02 +0100

    added Programming Fund - 25 April 2018 Part II

---
+++ Programming Fund - 25 April 2018 Part II/03. Tseam Account/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Tseam_Account
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> gameCollections = Console.ReadLine().Split(' ').ToList();
+
+            while (true)
+            {
+                string line = Console.ReadLine();
+
+                if (line == "Play!")
+                {
+                    break;
+                }
+
+
+                var commands = line.Split(' ');
+                var command = commands[0];
+                var gameName = commands[1];
+                if (command == "Install")
+                {
+                    if (!gameCollections.Contains(gameName))
+                    {
+                        gameCollections.Add(gameName);
+                    }
+
+                }
+                else if (command == "Uninstall")
+                {
+                    if (gameCollections.Contains(gameName))
+                    {
+                        gameCollections.Remove(gameName);
+                    }
+                }
+                else if (command == "Update")
+                {
+                    if (gameCollections.Contains(gameName))
+                    {
+                        gameCollections.Remove(gameName);
+                        gameCollections.Add(gameName);
+                    }
+                }
+                else if (command == "Expansion")
+                {
+                    var name = gameName.Split('-')[0];
+                    var expansion = gameName.Split('-')[1];
+                    if (gameCollections.Contains(name))
+                    {
+
+                        var index = gameCollections.IndexOf(name);
+                        gameCollections.Insert(index + 1, name+':'+expansion);
+                    }
+                }
+
+
+            }
+
+            Console.WriteLine(string.Join(" ", gameCollections));
+        }
+    }
+}
---
+++ Programming Fund - 25 April 2018 Part II/04. MOBA Challenger/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._MOBA_Challenger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            var playersSkills = new Dictionary<string, Dictionary<string, int>>();
+
+
+            while (input != "Season end")
+            {
+                if (input.Contains("->"))
+                {
+                    var line = input.Split(new string[]{" -> "}, StringSplitOptions.RemoveEmptyEntries);
+                    string player = line[0];
+                    string position = line[1];
+                    int skill = int.Parse(line[2]);
+                    if (!playersSkills.ContainsKey(player))
+                    {
+                        playersSkills.Add(player, new Dictionary<string, int>());
+                    }
+
+                    if (!playersSkills[player].ContainsKey(position))
+                    {
+                        playersSkills[player].Add(position, skill);
+                    }
+
+                    else
+                    {
+                        if (playersSkills[player][position] < skill)
+                        {
+                            playersSkills[player][position] = skill;
+                        }
+                    }
+                }
+
+                else
+                {
+                    string[] battle = input.Split(new string[] { " vs " }, StringSplitOptions.RemoveEmptyEntries);
+                    string firstUser = battle[0];
+                    string secondUser = battle[1];
+
+                    bool usersExist = CheckUsers(playersSkills, firstUser, secondUser);
+
+
+
+                    if (usersExist)
+                    {
+                        string samePosition = "Mortal Combat:)";
+                        while (samePosition != null)
+                        {
+                            samePosition = CheckSamePositions(playersSkills, firstUser, secondUser);
+                            if (samePosition != null)
+                            {
+                                Battle(playersSkills, samePosition, firstUser, secondUser);
+                            }
+                        }
+
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var player in playersSkills.Where(x => x.Value.Count != 0).OrderByDescending(x => x.Value.Values.Sum()).ThenBy(x => x.Key))
+            {
+                Console.WriteLine($"{player.Key}: {player.Value.Values.Sum()} skill");
+                foreach (var skill in player.Value.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+                {
+                    Console.WriteLine($"- {skill.Key} <::> {skill.Value}");
+                }
+            }
+        }
+
+        public static void Battle(Dictionary<string, Dictionary<string, int>> playersSkills, string samePosition, string firstUser, string secondUser)
+        {
+            if (playersSkills[firstUser][samePosition] > playersSkills[secondUser][samePosition])
+            {
+                playersSkills[secondUser].Remove(samePosition);
+            }
+            else if (playersSkills[firstUser][samePosition] < playersSkills[secondUser][samePosition])
+            {
+                playersSkills[firstUser].Remove(samePosition);
+            }
+        }
+
+        public static bool CheckUsers(Dictionary<string, Dictionary<string, int>> playersSkills, string firstUser, string secondUser)
+        {
+            if (playersSkills.ContainsKey(firstUser) && playersSkills.ContainsKey(secondUser))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static string CheckSamePositions(Dictionary<string, Dictionary<string, int>> playersSkills, string firstUser, string secondUser)
+        {
+            foreach (var firstPlayerPosition in playersSkills[firstUser])
+            {
+                foreach (var secondPlayerPosition in playersSkills[secondUser])
+                {
+                    if (firstPlayerPosition.Key == secondPlayerPosition.Key)
+                    {
+                        return firstPlayerPosition.Key;
+
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+}
