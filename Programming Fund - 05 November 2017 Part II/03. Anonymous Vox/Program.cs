c88ba970d3933c4719c1c1d5e7243ed9c9bab18b
theoners <theonebgrs@gmail.com>
2018-12-14 17:28:12 +0100

    added Programming Fund - 05 November 2017 Part II

---
+++ Programming Fund - 05 November 2017 Part II/03. Anonymous Vox/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace _03._Anonymous_Vox
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text.RegularExpressions;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string value = Console.ReadLine();
+            Regex textRegex = new Regex(@"([A-Za-z]+)(.+)\1");
+            Regex valueRegex = new Regex(@"{(.+?)}");
+            List<string> wordList = new List<string>();
+            List<string> valueList = new List<string>();
+
+            var placeHolders = textRegex.Matches(text);
+            var values = valueRegex.Matches(value);
+
+            if (!textRegex.IsMatch(text) && valueRegex.IsMatch(value))
+            {
+                Console.WriteLine(text);
+                return;
+            }
+            foreach (Match placeHolder in placeHolders)
+            {
+                wordList.Add(placeHolder.Groups[2].Value);
+            }
+
+            foreach (Match valueWord in values)
+            {
+                valueList.Add(valueWord.Groups[1].Value);
+            }
+
+            var count = Math.Min(wordList.Count, valueList.Count);
+
+            for (int i = 0; i < count; i++)
+            {
+                var index = text.IndexOf(wordList[i]);
+                text = text.Remove(index, wordList[i].Length);
+                text = text.Insert(index, valueList[i]);
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
---
+++ Programming Fund - 05 November 2017 Part II/04. Anonymous Cache/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _04._Anonymous_Cache
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> inputList= new List<string>();
+            var dataSets = new Dictionary<string, Dictionary<string, long>>();
+            List<string> dataKeys = new List<string>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "thetinggoesskrra")
+                {
+                    break;
+                }
+                inputList.Add(input);
+            }
+
+            for (int i = 0; i < inputList.Count; i++)
+            {
+                if (!inputList[i].Contains("->"))
+                {
+                    dataSets.Add(inputList[i],new Dictionary<string, long>());
+                    
+                }
+                else
+                {
+                    dataKeys.Add(inputList[i]);
+                }
+            }
+
+            for (int i = 0; i < dataKeys.Count; i++)
+            {
+                var dataKeySplit = dataKeys[i].Split(new string[] {"->", "|"}, StringSplitOptions.RemoveEmptyEntries);
+                var dataKey = dataKeySplit[0].Trim();
+                var dataSize = long.Parse(dataKeySplit[1].Trim());
+                var dataSet = dataKeySplit[2].Trim();
+
+                if (dataSets.ContainsKey(dataSet))
+                {
+                    dataSets[dataSet].Add(dataKey,dataSize);
+                }
+            }
+
+            foreach (var dataSet in dataSets.OrderByDescending(x=>x.Value.Values.Sum()))
+            {
+                Console.WriteLine($"Data Set: {dataSet.Key}, Total Size: {dataSet.Value.Values.Sum()}");
+                foreach (var dataKey in dataSet.Value)
+                {
+                    Console.WriteLine($"$.{dataKey.Key}");
+                }
+               break;
+            }
+        }
+    }
+}
