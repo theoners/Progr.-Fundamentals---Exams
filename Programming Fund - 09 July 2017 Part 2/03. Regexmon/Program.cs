a6fb384f01ca0ed2ce4afa09fed4014875384292
theoners <theonebgrs@gmail.com>
2018-12-15 09:57:58 +0100

    added Programming Fund - 09 July 2017 Part 2

---
+++ Programming Fund - 09 July 2017 Part 2/03. Regexmon/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _03._Regexmon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Regex didimonRegex = new Regex(@"[^A-Za-z-]+");
+            Regex bojomonRegex = new Regex(@"[A-Za-z]+-[a-zA-Z]+");
+
+            while (input.Length!=0)
+            {
+                var didimon = didimonRegex.Match(input).Value;
+                if (!didimonRegex.IsMatch(input))
+                {
+                    break;
+                }
+                Console.WriteLine(didimon);
+                var index = input.IndexOf(didimon);
+                input = input.Remove(0, index + didimon.Length);
+                var bojomon = bojomonRegex.Match(input).Value;
+                if (!bojomonRegex.IsMatch(input))
+                {
+                    break;
+                }
+                Console.WriteLine(bojomon);
+                index = input.IndexOf(bojomon);
+                input = input.Remove(0, index + bojomon.Length);
+            }
+        }
+    }
+}
---
+++ Programming Fund - 09 July 2017 Part 2/04. Pokemon Evolution/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace _04._Pokemon_Evolution
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Pokemon
+    {
+        public string EvolutionType { get; set; }
+        public int EvolutionIndex { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pokemonList = new Dictionary<string, List<Pokemon>>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "wubbalubbadubdub")
+                {
+                    break;
+                }
+
+                if (input.Contains("->"))
+                {
+                    var inputSplit = input.Split(new string[] { "->" }, StringSplitOptions.RemoveEmptyEntries);
+                    var name = inputSplit[0].Trim();
+                    var type = inputSplit[1].Trim();
+                    var index = int.Parse(inputSplit[2].Trim());
+
+                    if (!pokemonList.ContainsKey(name))
+                    {
+                        pokemonList.Add(name, new List<Pokemon>());
+                    }
+
+                    var pokemon = new Pokemon()
+                    {
+                        EvolutionType = type,
+                        EvolutionIndex = index
+                    };
+                    pokemonList[name].Add(pokemon);
+                }
+                else
+                {
+                    if (pokemonList.ContainsKey(input))
+                    {
+                        foreach (var pokemon in pokemonList.Where(x => x.Key == input))
+                        {
+                            Console.WriteLine($"# {pokemon.Key}");
+                            foreach (var pokemonProperties in pokemon.Value)
+                            {
+                                Console.WriteLine($"{pokemonProperties.EvolutionType} <-> {pokemonProperties.EvolutionIndex}");
+                            }
+                        }
+                    }
+                }
+            }
+
+            foreach (var pokemon in pokemonList)
+            {
+                Console.WriteLine($"# {pokemon.Key}");
+                foreach (var pokemonProperties in pokemon.Value.OrderByDescending(x => x.EvolutionIndex))
+                {
+                    Console.WriteLine($"{pokemonProperties.EvolutionType} <-> {pokemonProperties.EvolutionIndex}");
+                }
+            }
+        }
+    }
+}
