b8b4446eb9946903d8167bf736df5cc579fd0f8a
theoners <theonebgrs@gmail.com>
2018-12-17 09:46:45 +0200

    added Technology Fund Exam -16 December 2018

---
+++ Technology Fund Exam -16 December 2018/01. Concert/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Concert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> bandList = new Dictionary<string, List<string>>();
+            Dictionary<string, long> playTimes = new Dictionary<string, long>();
+            while (true)
+            {
+                var input = Console.ReadLine().Split(new string[] { ";", "," }, StringSplitOptions.RemoveEmptyEntries);
+
+                if (input[0] == "start of concert")
+                {
+                    break;
+                }
+
+                var command = input[0].Trim();
+                if (command == "Add")
+                {
+
+                    var bandName = input[1].Trim();
+                    if (!bandList.ContainsKey(bandName))
+                    {
+                        bandList.Add(bandName, new List<string>());
+
+                    }
+
+                    for (int i = 2; i < input.Length; i++)
+                    {
+                        var member = input[i].Trim();
+                        if (!bandList[bandName].Contains(member))
+                        {
+                            bandList[bandName].Add(member);
+                        }
+                    }
+                }
+                else
+                {
+                    var bandName = input[1].Trim();
+                    var playTime = long.Parse(input[2].Trim());
+
+                    if (!playTimes.ContainsKey(bandName))
+                    {
+                        playTimes.Add(bandName,0);
+                    }
+
+                    playTimes[bandName] += playTime;
+                }
+
+            }
+
+            var groupName = Console.ReadLine();
+
+            Console.WriteLine($"Total time: {playTimes.Values.Sum()}");
+            foreach (var kvp in playTimes.OrderByDescending(x=>x.Value).ThenBy(x=>x.Key))
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+
+            foreach (var band in bandList.Where(x=>x.Key==groupName))
+            {
+                Console.WriteLine(band.Key);
+                foreach (var member in band.Value)
+                {
+                    Console.WriteLine($"=> {member}");
+                }
+            }
+        }
+    }
+}
---
+++ Technology Fund Exam -16 December 2018/02. Song Encryption/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _02._Song_Encryption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Regex regex = new Regex(@"([A-Z '][a-z' ]+):([A-Z ]+)");
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+                if (input == "end")
+                {
+                    break;
+                }
+                var artist = regex.Match(input).Groups[1].Value;
+                var song = regex.Match(input).Groups[2].Value;
+                var match = regex.Match(input).Value;
+                var result = "";
+
+                if (match.Length==input.Length)
+                {
+                    for (int i = 0; i < artist.Length; i++)
+                    {
+                        if (artist[i] == ' ' || artist[i] == '\'')
+                        {
+                            result += artist[i];
+                        }
+
+                        else if (artist[i] == ':')
+                        {
+                            result += '@';
+                        }
+                        else
+                        {
+                            if (i == 0)
+                            {
+                                if (artist.Length + artist[i] > 90)
+                                {
+                                    result += (char)(artist[i] + artist.Length - 26);
+                                }
+                                else
+                                {
+                                    result += (char)(artist[i] + artist.Length);
+                                }
+                            }
+                            else
+                            {
+                                if (artist.Length + artist[i] > 122)
+                                {
+                                    result += (char)(artist[i] + artist.Length - 26);
+                                }
+                                else
+                                {
+                                    result += (char)(artist[i] + artist.Length);
+                                }
+                            }
+
+
+                        }
+                    }
+
+                    result += "@";
+
+                    for (int i = 0; i < song.Length; i++)
+                    {
+                        if (song[i] == ' ')
+                        {
+                            result += song[i];
+                        }
+
+                        else if (song[i] == ':')
+                        {
+                            result += '@';
+                        }
+                        else
+                        {
+
+                            if (artist.Length + song[i] > 90)
+                            {
+                                result += (char)(song[i] + artist.Length - 26);
+                            }
+                            else
+                            {
+                                result += (char)(song[i] + artist.Length);
+                            }
+
+                        }
+                    }
+                    Console.WriteLine($"Successful encryption: {result}");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input!");
+                }
+
+            }
+        }
+    }
+}
